fde31f0b636ba698a18e200ea5a9b758fa9a6c9d
Gaspard Vingtrinier <gaspard.vingtrinier@student-cs.fr>
2021-11-18 23:03:29 +0100

    [ADD] comments

--- tweet_collector/tweet.py
+++ tweet_collector/tweet.py
@@ -1,6 +1,11 @@
 
 class Tweet:
     def __init__(self, kafka_msg = None):
+        """
+        If a kafka message is given at the initialisation of the 
+        Tweet object, it is initialised with the information in 
+        the kafka message
+        """
         self.type = ""
         self.msg  = ""
         self.time = 0
@@ -12,14 +17,10 @@ class Tweet:
 
     def create_tweet(self, kafka_msg):
         """
-        In Python, the kafka msg.value is already a dictionnary,
-        no need to create a difficult string eater
-        A tweet is : {"type" : "tweet"|"retweet", 
-                "msg": "...", 
-                "time": timestamp,
-                "magnitude": 1085.0,
-                "source": 0,
-                "info": "blabla"}
+        Update the information of the Tweet object based on the message 
+        of a kafka producer.
+        Note: In Python, the kafka msg.value is already a dictionnary,
+        no need to create a difficult string eater.
         """
         self.type = kafka_msg.get("type")
         self.msg = kafka_msg.get("msg")
@@ -29,6 +30,9 @@ class Tweet:
         self.info = kafka_msg.get("info")
     
     def create_tweet_dict(self):
+        """
+        Create a pyton dictionnary with the Tweet information
+        """
         tweet_dict = dict()
         tweet_dict["type"] = self.type
         tweet_dict["msg"] = self.msg
@@ -39,5 +43,8 @@ class Tweet:
         return tweet_dict
     
     def display(self):
+        """
+        Print the tweet information in the form of a dictionnary
+        """
         print(self.create_tweet_dict())
 
