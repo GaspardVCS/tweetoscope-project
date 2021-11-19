e5b086d583c1ebdd7225f3d1707679ae0a48bda8
Gaspard Vingtrinier <gaspard.vingtrinier@student-cs.fr>
2021-11-19 14:26:40 +0100

    [ADD] send partial cascade and propertiesto kafka topics when needed

--- tweet_collector/cascade.py
+++ tweet_collector/cascade.py
@@ -1,11 +1,13 @@
 from tweet import Tweet
+from main_tweet_collector import producer
 
 class Cascade:
-    def __init__(self, tweet, terminated):
+    def __init__(self, tweet, params):
         self.identifier = tweet.cascade
+        self.starting_time = tweet.time
         self.first_message = tweet.msg
         self.rt_mag_time = [(tweet.time, tweet.magnitude)] # list of (magnitude, time) of retweet
-        self.terminated = int(terminated)
+        self.params = params
 
     def process_tweet(self, tweet):
         """
@@ -14,6 +16,10 @@ class Cascade:
         """
         self.rt_mag_time.append((tweet.time, tweet.magnitude))
         print(f"Tweet {tweet.id} processed at time {tweet.time}")
+        for o in self.params["observations"]:
+            if (tweet.time - self.starting_time) > o:
+                self.send_partial_cascade(o)
+                self.params["observations"].remove(o) # We don't want to send the same observations multiple times
     
     def is_terminated(self, tweet):
         """
@@ -21,4 +27,30 @@ class Cascade:
         terminated seconds before the current tweet. If so the cascade 
         should be deleted. 
         """
-        return (tweet.time - self.rt_mag_time[-1][0] > self.terminated)
+        if (tweet.time - self.rt_mag_time[-1][0] > self.params["terminated"]):
+            self.send_cascade_properties()
+            return True
+        return False
+    
+    def send_partial_cascade(self, observation):
+        """
+        Send the partial cascade for the observation window to the 
+        kafka topic 'cascade_series'
+        """
+        msg = {
+            "observation_window": observation,
+            "partial_cascade": [elem for elem in self.rt_mag_time \
+                                if elem[0] - self.starting_time < observation],
+        }
+        producer.send('cascade_series', key=self.identifier, value=msg) # Send a new message to topic
+        producer.flush() # not sure if necessary or not
+    
+    def send_cascade_properties(self):
+        """
+        Send the cascade properties to the kafka topic 'cascade_properties'
+        """
+        msg = {
+            "cascade": self.rt_mag_time,
+        }
+        producer.send('cascade_properties', key=self.identifier, value=msg) # Send a new message to topic
+        producer.flush() # not sure if necessary or not
