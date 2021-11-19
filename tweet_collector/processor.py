b908eedcd0c917e1c01b56ae86c4645a94a7a368
Gaspard Vingtrinier <gaspard.vingtrinier@student-cs.fr>
2021-11-19 11:45:52 +0100

    [ADD] prioriry queue

--- tweet_collector/processor.py
+++ tweet_collector/processor.py
@@ -6,25 +6,23 @@ class Processor:
     def __init__(self, source, params):
         self.source = source
         self.params = params
-        self.cascade_queue = list() # see queue in python, list for now
-        #TODO: change list to queue
+        self.cascade_queue = list()
     
     def process_tweet(self, tweet):
         tweet_processed = False
-        for c in self.cascade_queue:
-            #TODO: add deletion of cascade if timestamp
+        # Traverse the queue in reverse
+        for i, (t, c) in zip(range(len(self.cascade_queue) - 1, -1, -1), self.cascade_queue[::-1]):
             if tweet.cascade == c.identifier:
                 c.process_tweet(tweet)
                 tweet_processed = True
+                new_cascade = self.cascade_queue.pop(i)
+                self.cascade_queue.append((tweet.time, new_cascade[1]))
                 break
-            # Delete cascade if it is too old
+            # Delete cascade if the last tweet in the cascade is too old
             if c.is_terminated(tweet):
                 print(f"Cascade {c.identifier} has been deleted")
-                self.cascade_queue.remove(c)
+                self.cascade_queue.remove((t, c))
                 del c
-
-
         if not tweet_processed and tweet.type == "tweet":
-            self.cascade_queue.append(Cascade(tweet, self.params["terminated"]))
+            self.cascade_queue.append((tweet.time, Cascade(tweet, self.params["terminated"])))
             print(f"Cascade {tweet.cascade} added to Processor {self.source}")
-
