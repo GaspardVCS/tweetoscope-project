c83d8dca6eeca97ce4502311206db78ea83d659e
Gaspard Vingtrinier <gaspard.vingtrinier@student-cs.fr>
2021-11-19 01:43:44 +0100

    [ADD] process_tweet function, still need to work on it

--- tweet_collector/processor.py
+++ tweet_collector/processor.py
@@ -1,5 +1,20 @@
-
+from tweet import Tweet
+from cascade import Cascade
 
 class Processor:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, source):
+        self.source = source
+        self.cascade_queue = list() # see queue in python, list for now
+    
+    def process_tweet(self, tweet):
+        cascade_identifier = tweet.cascade
+        tweet_processed = False
+        for c in self.cascade_queue:
+            if cascade_identifier == c.identifier:
+                c.process_tweet(tweet)
+                tweet_processed = True
+                break
+        if not tweet_processed:
+            self.cascade_queue.append(Cascade(tweet))
+            print(f"Cascade {tweet.cascade} added to Processor {self.source}")
+
