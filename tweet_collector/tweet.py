a7fbf6bd403bd231efe2172b63a3c1e82a784bd3
Gaspard Vingtrinier <gaspard.vingtrinier@student-cs.fr>
2021-11-19 01:44:34 +0100

    [FIX] typo, forgot = in split

--- tweet_collector/tweet.py
+++ tweet_collector/tweet.py
@@ -30,7 +30,7 @@ class Tweet:
         self.magnitude = kafka_msg.value.get("m")
         self.source = kafka_msg.key
         self.info = kafka_msg.value.get("info")
-        self.cascade = int(self.info.split("")[1])
+        self.cascade = int(self.info.split("=")[1])
         self.id = kafka_msg.value.get("tweet_id")
     
     def create_tweet_dict(self):
