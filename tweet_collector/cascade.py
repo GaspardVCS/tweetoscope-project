7c86faff6ae24e6f289b07e0b02258ac17f72f31
Gaspard Vingtrinier <gaspard.vingtrinier@student-cs.fr>
2021-11-19 10:09:23 +0100

    [ADD] delete cascade if it is terminated because of time

--- tweet_collector/cascade.py
+++ tweet_collector/cascade.py
@@ -1,10 +1,20 @@
 from tweet import Tweet
 
 class Cascade:
-    def __init__(self, tweet):
+    def __init__(self, tweet, terminated):
         self.identifier = tweet.cascade
         self.first_message = tweet.msg
-        self.rt_mag_time = list() # list of (magnitude, time) of retweet
-    
+        self.rt_mag_time = [(tweet.time, tweet.magnitude)] # list of (magnitude, time) of retweet
+        self.terminated = int(terminated)
+
     def process_tweet(self, tweet):
-        print(f"Tweet {tweet.id} processed!")
+        self.rt_mag_time.append((tweet.time, tweet.magnitude))
+        print(f"Tweet {tweet.id} processed at time {tweet.time}!")
+    
+    def is_terminated(self, tweet):
+        """
+        Return True if the last tweet in the cascade was seen more than
+        terminated seconds before the current tweet. If so the cascade 
+        should be deleted. 
+        """
+        return (tweet.time - self.rt_mag_time[-1][0] > self.terminated)
--- tweet_collector/processor.py
+++ tweet_collector/processor.py
@@ -1,20 +1,30 @@
 from tweet import Tweet
+import time
 from cascade import Cascade
 
 class Processor:
-    def __init__(self, source):
+    def __init__(self, source, params):
         self.source = source
+        self.params = params
         self.cascade_queue = list() # see queue in python, list for now
+        #TODO: change list to queue
     
     def process_tweet(self, tweet):
-        cascade_identifier = tweet.cascade
         tweet_processed = False
         for c in self.cascade_queue:
-            if cascade_identifier == c.identifier:
+            #TODO: add deletion of cascade if timestamp
+            if tweet.cascade == c.identifier:
                 c.process_tweet(tweet)
                 tweet_processed = True
                 break
-        if not tweet_processed:
-            self.cascade_queue.append(Cascade(tweet))
+            # Delete cascade if it is too old
+            if c.is_terminated(tweet):
+                print(f"Cascade {c.identifier} has been deleted")
+                self.cascade_queue.remove(c)
+                del c
+
+
+        if not tweet_processed and tweet.type == "tweet":
+            self.cascade_queue.append(Cascade(tweet, self.params["terminated"]))
             print(f"Cascade {tweet.cascade} added to Processor {self.source}")
 
