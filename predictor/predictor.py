8c55dac166ec9375135b22f534aa604c7f4dcad9
Gaspard Vingtrinier <gaspard.vingtrinier@student-cs.fr>
2021-11-20 19:09:32 +0100

    [ADD] skeleton of V1 predictor

---
+++ predictor/predictor.py
@@ -0,0 +1,17 @@
+
+
+class Predictor:
+    def __init__(self, observation_window):
+        self.key = observation_window
+
+    def predict(self):
+        pass
+
+    def send_sample(self):
+        pass
+
+    def send_alert(self):
+        pass
+
+    def send_stat(self):
+        pass
