652e1690c923be27f6712a022fd593b0bb500e82
Gaspard Vingtrinier <gaspard.vingtrinier@student-cs.fr>
2021-11-20 18:06:54 +0100

    [FIX] messages send to cascade_properties

--- tweet_collector/cascade.py
+++ tweet_collector/cascade.py
@@ -8,7 +8,8 @@ class Cascade:
         self.starting_time = tweet.time
         self.first_message = tweet.msg
         self.rt_mag_time = [(tweet.time, tweet.magnitude)] # list of (magnitude, time) of retweet
-        self.params = copy.deepcopy(params)
+        self.params = params
+        self.observations_to_send = copy.deepcopy(params["observations"])
 
     def process_tweet(self, tweet):
         """
@@ -17,10 +18,10 @@ class Cascade:
         """
         self.rt_mag_time.append((tweet.time, tweet.magnitude))
         # print(f"Tweet {tweet.id} processed at time {tweet.time}")
-        for o in self.params["observations"]:
+        for o in self.observations_to_send:
             if (tweet.time - self.starting_time) > int(o):
                 self.send_partial_cascade(o)
-                self.params["observations"].remove(o) # We don't want to send the same observations multiple times
+                self.observations_to_send.remove(o) # We don't want to send the same observations multiple times
 
     def is_terminated(self, tweet):
         """
@@ -32,7 +33,7 @@ class Cascade:
             and (len(self.rt_mag_time) >= self.params["min_cascade_size"]):
             # if there are still observations windows that weren't send, we send them
             # NOTE: maybe send them only at the end of the cascade?
-            for o in self.params["observations"]: 
+            for o in self.observations_to_send: 
                 self.send_partial_cascade(o)
             self.send_cascade_properties()
             return True
@@ -58,8 +59,12 @@ class Cascade:
         """
         Send the cascade properties to the kafka topic 'cascade_properties'
         """
-        msg = {
-            "number_retweet": len(self.rt_mag_time),
-        }
-        producer.send(self.params["out_properties"], key=str(self.identifier), value=msg) # Send a new message to topic
-        producer.flush() # not sure if necessary or not
+        for observation in self.params["observations"]:
+            msg = {
+                "type": "size",
+                "cid": self.identifier,
+                "n_tot": len(self.rt_mag_time),
+                "t_end": self.rt_mag_time[-1][0],
+            }
+            producer.send(self.params["out_properties"], key=observation, value=msg) # Send a new message to topic
+            producer.flush() # not sure if necessary or not
