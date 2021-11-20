0ab498cbc417e8c280b2f09d2379921cb51632d7
Gaspard Vingtrinier <gaspard.vingtrinier@student-cs.fr>
2021-11-20 15:58:18 +0100

    [FIX] priority queue

--- tweet_collector/processor.py
+++ tweet_collector/processor.py
@@ -11,16 +11,16 @@ class Processor:
     def process_tweet(self, tweet):
         tweet_processed = False
         # Traverse the queue in reverse
-        for i, (t, c) in zip(range(len(self.cascade_queue) - 1, -1, -1), self.cascade_queue[::-1]):
+        for (t, c) in self.cascade_queue:
             if tweet.cascade == c.identifier:
                 c.process_tweet(tweet)
                 tweet_processed = True
-                new_cascade = self.cascade_queue.pop(i)
-                self.cascade_queue.append((tweet.time, new_cascade[1]))
+                self.cascade_queue.remove((t, c))
+                self.cascade_queue.append((tweet.time, c))
                 break
             # Delete cascade if the last tweet in the cascade is too old
             if c.is_terminated(tweet):
-                print(f"Cascade {c.identifier} has been deleted")
+                print(f"Cascade {c.identifier} has been deleted with length {len(c.rt_mag_time)}")
                 self.cascade_queue.remove((t, c))
                 del c
         if not tweet_processed and tweet.type == "tweet":
