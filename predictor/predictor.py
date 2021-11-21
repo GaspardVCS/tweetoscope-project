b4a3b28d7204636318bd4f8fa5c379ca47a17e0b
Gaspard Vingtrinier <gaspard.vingtrinier@student-cs.fr>
2021-11-21 14:25:54 +0100

    [ADD] message send to stat topic + fix

--- predictor/predictor.py
+++ predictor/predictor.py
@@ -1,3 +1,4 @@
+from copy import Error
 from kafka import KafkaProducer
 
 class Predictor:
@@ -7,24 +8,11 @@ class Predictor:
         self.cascade_map = dict()
     
     def process_message(self, message):
+        self.update_map(message)
+        cid = message["cid"]
         if message["type"] == "size":
-            self.update_map(message)
-            prediction = self.predict(message)
-
-            alert_key = ""
-            alert_message = {
-                "type": "alert",
-                "cid": message["cid"],
-                "msg": None,
-                "T_obs": self.key,
-                "n_tot": prediction,
-            }
-            self.producer.send("alert", key=alert_key, value=alert_message) # Send a new message to topic
-            self.producer.flush() # not sure if necessary or not
-
-            # TODO: update map to send samples to sample topic
+            # Send message to the sample topic for training
             sample_key = self.key
-            cid = message["cid"]
             sample_message = {
                 "type": "sample",
                 "cid": cid,
@@ -32,33 +20,66 @@ class Predictor:
                 "W": self.cascade_map[cid]["W"]
             }
 
-            # TODO: send message with stat to the stat topic
+            self.producer.send("sample", key=sample_key, value=sample_message) # Send a new message to topic
+            self.producer.flush() # not sure if necessary or not
 
+            # Send the prediction error to the stat topic
+            n_true = self.cascade_map[cid]["n_tot"]
+            n_pred = self.cascade_map[cid]["prediction"]
+            stat_key = ""
+            stat_message = {
+                "type": "stat",
+                "cid": cid,
+                "T_obs": self.key,
+                "ARE": self.compute_are(n_pred, n_true),
+            }
+
+            self.producer.send("stat", key=stat_key, value=stat_message) # Send a new message to topic
+            self.producer.flush() # not sure if necessary or not
+        
         elif message["type"] == "parameters":
-            # TODO: update map to create the samples that will be send to the sample topic
-            pass
-        else:
-            print("ERROR") # add true error
+            # Get the prediction
+            prediction = self.predict(message)
+            self.cascade_map[cid]["prediction"] = prediction
+            # Send message to the alert topic
+            alert_key = ""
+            alert_message = {
+                "type": "alert",
+                "cid": cid,
+                "msg": None,
+                "T_obs": self.key,
+                "n_tot": prediction,
+            }
+            self.producer.send("alert", key=alert_key, value=alert_message) # Send a new message to topic
+            self.producer.flush() # not sure if necessary or not
+
 
     def predict(self, message):
-        prediction = message["n_prob"]
+        prediction = message["n_supp"]
         return prediction
 
     def update_map(self, message):
         cid = message["cid"]
-        if message["type"] == "params":
+        if message["type"] == "parameters":
             self.cascade_map[cid] = {
                 "params": message["params"],
                 "cascade_serie": message["tweets"],
             }
         elif message["type"] == "size":
             if cid in self.cascade_map:
-                self.map[cid]["n_tot"] = message["n_tot"]
+                self.cascade_map[cid]["n_tot"] = message["n_tot"]
                 W = self.find_true_omega(self.cascade_map[cid])
-                self.map[cid]["W"] = W
-    
-    def find_true_omega(self, cascade_dict):
+                self.cascade_map[cid]["W"] = W
+        else:
+            raise Error # Not sure about the syntax
+
+    @staticmethod
+    def find_true_omega(cascade_dict):
         _, _, n_star, G1 = cascade_dict["params"]
         W = cascade_dict["n_tot"] - len(cascade_dict["cascade_serie"])
         W *= (1 - n_star) / G1
         return W 
+
+    @staticmethod
+    def compute_are(n_pred, n_true):
+        return abs(n_true - n_pred) / n_true
