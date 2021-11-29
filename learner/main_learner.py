aeebec8f480fd01a44a6e79c3f9061de3c5bf99d
Gaspard Vingtrinier <gaspard.vingtrinier@student-cs.fr>
2021-11-29 22:55:23 +0100

    [UPDATE] main_learner function

--- learner/main_learner.py
+++ learner/main_learner.py
@@ -2,10 +2,10 @@ from kafka import KafkaConsumer, KafkaProducer
 import json
 from learner import Learner
 
-consumer = KafkaConsumer("sample",                             # Topic name
-  bootstrap_servers = "localhost:9092",                 # List of brokers passed from the command line
-  value_deserializer=lambda v: json.loads(v.decode('utf-8')),  # How to deserialize the value from a binary buffer
-  key_deserializer= lambda v: v.decode()                       # How to deserialize the key (if any)
+consumer = KafkaConsumer("sample",                       
+  bootstrap_servers = "localhost:9092",
+  value_deserializer=lambda v: json.loads(v.decode('utf-8')),
+  key_deserializer= lambda v: v.decode()
 )
 
 if __name__ =="__main__":
@@ -14,8 +14,4 @@ if __name__ =="__main__":
         if msg.key not in learner_map:
             learner_map[msg.key] = Learner(msg.key)
         if msg.value["type"] == "sample":
-            print(msg.key)
             learner_map[msg.key].process_sample(msg.value)
-            if len(learner_map[msg.key].X) > 3:
-                prediction = learner_map[msg.key].model.predict([msg.value["X"]])
-                print("prediction", prediction)
