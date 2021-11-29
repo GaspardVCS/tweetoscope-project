2e3c7fac3cf5cd0b9f8a6882242667f52a711db5
Gaspard Vingtrinier <gaspard.vingtrinier@student-cs.fr>
2021-11-29 22:54:15 +0100

    [ADD] model topic consumer

---
+++ kafka_consumers/model_consumer.py
@@ -0,0 +1,15 @@
+from kafka import KafkaConsumer, KafkaProducer
+import pickle
+
+
+consumer = KafkaConsumer('model',                             # Topic name
+  bootstrap_servers = "localhost:9092",                 # List of brokers passed from the command line
+  value_deserializer=lambda v: pickle.loads(v),  # How to deserialize the value from a binary buffer
+  key_deserializer= lambda v: v.decode()                       # How to deserialize the key (if any)
+)
+
+for msg in consumer:
+    print(msg.key, msg.value)
+    X = [[1.022, 2.36, 5.24, 0.2225]]
+    prediction = msg.value.predict(X)
+    print(prediction)
