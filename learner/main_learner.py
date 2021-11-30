d3b73d5a9eff817ba33adf02687119ceed49ef8b
Gaspard Vingtrinier <gaspard.vingtrinier@student-cs.fr>
2021-11-30 11:34:05 +0100

    [CLEAN] add comments

--- learner/main_learner.py
+++ learner/main_learner.py
@@ -3,16 +3,22 @@ import json
 from params import params
 from learner import Learner
 
+# Initialize the Kafka consumer for the sample topic
 consumer = KafkaConsumer("sample",                       
   bootstrap_servers = params["brokers"],
   value_deserializer=lambda v: json.loads(v.decode('utf-8')),
   key_deserializer= lambda v: v.decode()
 )
 
-if __name__ =="__main__":
+def main():
+    # Map where the key is the time window and the value a learner object  
     learner_map = dict()
     for msg in consumer:
         if msg.key not in learner_map:
             learner_map[msg.key] = Learner(msg.key)
         if msg.value["type"] == "sample":
             learner_map[msg.key].process_sample(msg.value)
+
+
+if __name__ =="__main__":
+    main()
